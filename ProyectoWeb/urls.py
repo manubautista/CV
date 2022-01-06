3b597170f698ecd1230747d034eefd9ce2b8a807
manubautista <manubautista2009@gmail.com>
2022-01-06 00:32:50 -0300

    Upload

---
+++ ProyectoWeb/urls.py
@@ -0,0 +1,16 @@
+#urls del proyecto. se borró el import de las vistas y los url patterns fueron movidos a urls.py de la app.
+from django.conf.urls import include
+from django.contrib import admin
+from django.urls import path, include
+
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+
+
+    
+
+    
+    
+    path('', include('ProyectoWebApp.urls')),
+]
